ce510e9fd8ccf887fb8282c0a98a76858585b780
ToriLutz <vglutz@comcast.net>
2025-03-08 18:10:24 -0800

    bom
    man JS makes me feel stupid

--- wdd131/scripts/getdates.js
+++ wdd131/scripts/getdates.js
@@ -4,3 +4,7 @@ let year = document.getElementById("currentYear");
 year.innerHTML = currentYear;
 const modifiedYear = document.lastModified;
 document.getElementById('lastModified').innerText = modifiedYear;
+li.textContent = input.variable;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
---
+++ week02/scripts/scripts.js
@@ -0,0 +1,7 @@
+const button= document.querySelector('button');
+const input = document.querySelector('#favchap');
+const list=document.querySelector('______');
+const li= document.createElement('li');
+const deleteButton = document.createElement('button');
+
+
