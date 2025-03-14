90f1422d693ba401bd7064b727624667d14bf429
ToriLutz <vglutz@comcast.net>
2025-03-14 14:18:23 -0700

    updated

--- wdd131/scripts/temples.js
+++ wdd131/scripts/temples.js
@@ -1,19 +1,19 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
-
-
-hamburger.addEventListener('click', () => {hamburger.classList.toggle("active");})
-navMenu.addEventListener('click', () => {navMenu.classList.toggle("active");})
-
-
-
-
 const currentYear = new Date().getFullYear();
 let year = document.getElementById("currentYear");
 year.innerHTML = currentYear;
 const modifiedYear = document.lastModified;
-document.getElementById('lastModified').innerText = modifiedYear;
-li.textContent = input.variable;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active")}; {
+    navMenu.classList.toggle("active");});
+
+
+
+
+
+
+
