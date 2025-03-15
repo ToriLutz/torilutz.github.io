332e082c846713e3d7b15fe66ae4d62cdb706411
ToriLutz <vglutz@comcast.net>
2025-03-14 21:56:20 -0700

    Update temples.js

--- wdd131/scripts/temples.js
+++ wdd131/scripts/temples.js
@@ -12,6 +12,7 @@ const currentYear = new Date().getFullYear();
 let year = document.getElementById("currentYear");
 year.innerHTML = currentYear;
 const modifiedYear = document.lastModified;
+document.getElementById('lastModified').innerText = modifiedYear;
 
 
 
