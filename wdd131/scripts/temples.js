e1b7a045ed7cb94b7bf8f8cff6fb0cc0f8251035
ToriLutz <vglutz@comcast.net>
2025-03-14 21:48:22 -0700

    got it working

--- wdd131/scripts/temples.js
+++ wdd131/scripts/temples.js
@@ -1,17 +1,17 @@
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");         
+})
+
+
 const currentYear = new Date().getFullYear();
 let year = document.getElementById("currentYear");
 year.innerHTML = currentYear;
 const modifiedYear = document.lastModified;
-
-
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle("active")}; {
-    navMenu.classList.toggle("active");});
-
-
 
 
 
