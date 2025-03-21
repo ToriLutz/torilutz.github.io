004eabc007daf224180a0639c1f21329c80c0084
ToriLutz <vglutz@comcast.net>
2025-03-20 21:00:21 -0700

    added JS windChill function and media Queries

---
+++ wdd131/scripts/place.js
@@ -0,0 +1,11 @@
+var temp = 10;
+var wSpeed = 5;
+var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+var windChill= Math.round(windChill);
+
+if (temp == 10 && wSpeed > 4.8){
+    document.getElementById("windChill").innerHTML = windChill
+  } else {
+    document.getElementById("windChill").innerHTML = "N/A";
+  }
