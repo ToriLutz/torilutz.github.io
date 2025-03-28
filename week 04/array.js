aa16e1bf23d4d7b051e9f53a86f39554f1265520
ToriLutz <vglutz@comcast.net>
2025-03-27 19:35:59 -0700

    improving temple website

---
+++ week 04/array.js
@@ -0,0 +1,7 @@
+let names = ['Nancy','Blessing','Jorge','Svetlana','Bob']
+
+// let results = names.filter((word) => nam.charAt (0) === 'b')
+
+// let namesLength = nam.map((name) = names.length)
+
+let reduce = names.reduce((names) => total + names.length, 0) / names.length; 
