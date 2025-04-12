f40d5fd0ad25f81263e9e7e7e83cffcfbe1d2cd9
ToriLutz <vglutz@comcast.net>
2025-04-11 20:17:54 -0700

    css

--- wdd131/project/scripts/scripts.js
+++ wdd131/project/scripts/scripts.js
@@ -18,6 +18,14 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");         
 })
 
+//last modified year
+const currentYear = new Date().getFullYear();
+let year = document.getElementById("currentYear");
+year.innerHTML = currentYear;
+const modifiedYear = document.lastModified;
+document.getElementById('lastModified').innerText = modifiedYear;
+
+
 
 //my product shop cards
 const products = [
